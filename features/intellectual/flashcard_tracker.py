0fc5a07da6c74fb57f08eab004faf99ba6bb1de0
Santy2206 <santiago2006172@gmail.com>
2026-06-19 16:19:44 -0500

    Implement weight tracking feature and refactor main application logic

---
+++ features/intellectual/flashcard_tracker.py
@@ -0,0 +1,13 @@
+import os
+from datetime import datetime, timedelta
+
+
+def main():
+    remnote_file_history = (
+        r"C:\Users\USUARIO\AppData\Roaming\RemNote\Local Storage\leveldb"
+    )
+    os.startfile(remnote_file_history)
+
+
+if __name__ == "__main__":
+    main()
